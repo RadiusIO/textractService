d9796c850e43b63f8236635b9d295db462e887dd
Christoph Grader <christoph.grader@protonmail.com>
2023-03-04 14:55:44 +0100

    Add spring boot starter integration

--- Document_Extraction_Service/src/main/java/me/cgrader/Main.java
+++ Document_Extraction_Service/src/main/java/me/cgrader/Main.java
@@ -1,7 +1,31 @@
 package me.cgrader;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.textract.TextractClient;
+
+import static me.cgrader.textract.AnalyzeDocument.analyzeDoc;
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+
+		String sourceDoc;
+//		sourceDoc = args[0];
+//		sourceDoc = "./testfiles/bermuda.png";
+//		sourceDoc = "./testfiles/sciencepaper.png";
+		sourceDoc = "./testfiles/diagram.jpeg";
+		Region region = Region.US_EAST_2;
+		TextractClient textractClient = TextractClient.builder()
+				.region(region)
+				.credentialsProvider(ProfileCredentialsProvider.create())
+				.build();
+
+		analyzeDoc(textractClient, sourceDoc);
+		textractClient.close();
+	}
 }
---
+++ Document_Extraction_Service/src/test/java/me/cgrader/Document_Extraction_Service/MainTests.java
@@ -0,0 +1,13 @@
+package me.cgrader.Document_Extraction_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
